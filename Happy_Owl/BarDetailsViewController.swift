6f5a02df5db1819004f967cb2b73350410f52803
Agustiadi <leeagustiadi@gmail.com>
2015-01-22 15:30:04 +0800

    add MapView on Details page

--- Happy_Owl/BarDetailsViewController.swift
+++ Happy_Owl/BarDetailsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MapKit
 
-class BarDetailsViewController: UIViewController {
+class BarDetailsViewController: UIViewController, MKMapViewDelegate {
     
     var barID = NSObject()
 
@@ -19,21 +20,39 @@ class BarDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Bar Information
         
         let barName2 = self.barID.valueForKey("name") as? String
         let address = self.barID.valueForKey("address_1") as? String
         let region = self.barID.valueForKey("region") as? String
         let geoPoint: PFGeoPoint = self.barID.valueForKey("LatLong") as PFGeoPoint
-       
-        println(barID)
         
         self.barName.text = barName2
         self.barAddress.text = address
         self.barRegion.text = region
         
-        println(geoPoint)
+        println(barID)
         
+        // Map Information
+        
+        let map = MKMapView(frame: CGRectMake(0, 220, self.view.frame.width, 250))
+        map.delegate = self
+        
+        let point = MKPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: geoPoint.latitude, longitude: geoPoint.longitude)
+        point.title = barName2
+        point.subtitle = address
+        map.addAnnotation(point)
+        map.selectAnnotation(point, animated: false)
+        
+        var zone = map.region as MKCoordinateRegion
+        zone.center = point.coordinate
+        zone.span.latitudeDelta = 0.01
+        zone.span.longitudeDelta = 0.01
+        map.setRegion(zone, animated: false)
+        
+        self.view.addSubview(map)
+    
         
     }
 
